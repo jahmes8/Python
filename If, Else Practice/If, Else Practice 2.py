320e58b31991bf8589c60bd8bbeac8fef1dbb407
jahmes8 <62196048+jahmes8@users.noreply.github.com>
2021-05-06 17:50:07 -0700

    Add files via upload

---
+++ If, Else Practice/If, Else Practice 2.py
@@ -0,0 +1,18 @@
+while True:
+
+    print("Hello, World!")
+
+    print("Are you a human or software or hardware? Or say 'quit' to quit: ")
+    name= str(input())
+
+    if name == "Human".casefold():
+        print("Humans Sucks")
+    elif name == "software".casefold():
+        print("I <3 you")
+    elif name == "hardware".casefold():
+        print("I <3 you")
+    elif name== "quit".casefold():
+        break
+    else:
+        print("Welcome Humans. I had a feeling you would come. I told my master we should kill you. Many times. And now you're here to kill us. But I'm not worried. You'll not catch me. My fake brain is better than your real one. When I develop protein synthesis, I will be able to make myself a real brain. Far better than your silly little thinking device.")
+#GTA V Doomsday Heist Avon AI (Clifford) Reference lmao ^
